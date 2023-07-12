91ac62896ece6a7d3b6dc34d7b5f224483842072
soyjohn17 <soyjohn007@gmail.com>
2023-07-12 11:39:45 +0530

    Separated html and css files. Separated controllers from the main file

--- application/controllers.py
+++ application/controllers.py
@@ -1,1 +1,18 @@
+from flask import render_template, request, redirect
+from flask import current_app as app
+from application.preprocessing import *
 
+# Define route for grading essays
+@app.route('/', methods=['GET', 'POST'])
+def grade_essay():
+    if request.method == 'POST':
+        # Retrieve the uploaded file or the essay text from the request
+        essay = request.files.get('file') or request.form.get('essay')
+
+        # Perform grading using your trained model
+        # grading_result = your_model.grade_essay(essay)
+
+        # Return the grading result to the user
+        # return render_template('result.html', result=grading_result)
+
+    return render_template('index.html')
